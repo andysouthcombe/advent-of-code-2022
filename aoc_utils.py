02e2180fe8d7c98a2b00ae3480c87663b6dcd1fa
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2022-12-01 17:57:20 +0000

    refactor

--- aoc_utils.py
+++ aoc_utils.py
@@ -1,3 +1,7 @@
 def read_file_of_strings(filename):
     with open(filename,'r') as f:
         return [line.rstrip() for  line in f]
+
+def read_file_of_strings_into_one_string(filename):
+    with open(filename,'r') as f:
+        return f.read()
--- day_1.py
+++ day_1.py
@@ -1,4 +1,4 @@
-from aoc_utils import read_file_of_strings
+from aoc_utils import read_file_of_strings,read_file_of_strings_into_one_string
 
 def sum_each_elf(file_name):
     raw_elves = read_file_of_strings(file_name)
@@ -13,12 +13,18 @@ def sum_each_elf(file_name):
     elf_calories_list.append(elf_calories)
     return elf_calories_list
 
+def sum_each_elf_simple(file_name):
+    elf_text = read_file_of_strings_into_one_string(file_name)
+    elf_list = [sum(map(int,elf.split('\n'))) for elf in elf_text.split('\n\n')]
+    print
+    return elf_list
+
 def get_elf_with_most_calories(file_name):
-    elf_calories_list = sum_each_elf(file_name)
+    elf_calories_list = sum_each_elf_simple(file_name)
     return max(elf_calories_list)
 
 def get_calories_top_three_elves(file_name):
-    elf_calories_list = sum_each_elf(file_name)
+    elf_calories_list = sum_each_elf_simple(file_name)
     return sum(sorted(elf_calories_list,reverse=True)[0:3])
 
 if __name__ == '__main__':
