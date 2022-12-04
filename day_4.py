38ceb0a1325b76bc667dde2e2512ae386a981578
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2022-12-04 10:44:23 +0000

    passes day 4

--- day_4.py
+++ day_4.py
@@ -1,4 +1,29 @@
 from aoc_utils import read_file_of_strings
 
 def read_assignments(filename):
-    return [assignment.split(',') for assignment in read_file_of_strings(filename)]
+    return [assignments.split(',') for assignments in read_file_of_strings(filename)]
+
+def expand_assignment_range(assignments):
+    range_one_text = assignments[0].split('-')
+    range_two_text = assignments[1].split('-')
+    range_one_int = [int(range_one) for range_one in range_one_text]
+    range_two_int = [int(range_one) for range_one in range_two_text]
+    return [list(range(range_one_int[0],range_one_int[1]+1)),list(range(range_two_int[0],range_two_int[1]+1))]
+
+def do_assignments_fully_overlap(assignments):
+    return set(assignments[0]).issuperset(set(assignments[1])) or set(assignments[1]).issuperset(set(assignments[0]))
+
+def do_assignments_overlap_at_all(assignments):
+    return len(set(assignments[0]).intersection(set(assignments[1]))) > 0
+
+def count_fully_overlapping_ranges(assignments):
+    assignment_ranges = [expand_assignment_range(assignment) for assignment in assignments]
+    return sum([int(do_assignments_fully_overlap(ass_range)) for ass_range in assignment_ranges])
+
+def count_any_overlapping_ranges(assignments):
+    assignment_ranges = [expand_assignment_range(assignment) for assignment in assignments]
+    return sum([int(do_assignments_overlap_at_all(ass_range)) for ass_range in assignment_ranges])
+
+if __name__ == '__main__':
+    print(count_fully_overlapping_ranges(read_assignments('input/day_4_input.txt')))
+    print(count_any_overlapping_ranges(read_assignments('input/day_4_input.txt')))
--- day_4_test.py
+++ day_4_test.py
@@ -1,8 +1,29 @@
 import pytest
-from day_4 import read_assignments  
+from day_4 import read_assignments, expand_assignment_range, do_assignments_fully_overlap, count_fully_overlapping_ranges, do_assignments_overlap_at_all
 
 test_assignments = read_assignments('input/day_4_test_input.txt')
 
 def test_file_read_ok():
     assert test_assignments[0] == ['2-4','6-8']
     assert test_assignments[5] == ['2-6','4-8']
+
+def test_single_digit_range_expanded_ok():
+    assert expand_assignment_range(['2-4','6-8']) == [[2,3,4],[6,7,8]]
+
+def test_two_digit_range_expanded_ok():
+    assert expand_assignment_range(['2-4','6-10']) == [[2,3,4],[6,7,8,9,10]]
+
+def test_non_overlapping_range_returns_false():
+    assert do_assignments_fully_overlap([[2,3,4],[6,7,8]]) == False
+
+def test_fully_overlapping_range_returns_true():
+    assert do_assignments_fully_overlap([[7,8],[6,7,8,9,10]]) == True
+
+def test_count_fully_overlapping_assignments():
+    assert count_fully_overlapping_ranges(test_assignments) == 2
+
+def test_assignment_no_overlap_at_all():
+    assert do_assignments_overlap_at_all([[2,3,4],[6,7,8]]) == False
+
+def test_assignment_some_overlap():
+    assert do_assignments_overlap_at_all([[2,3,4],[4,5,6]]) == True
