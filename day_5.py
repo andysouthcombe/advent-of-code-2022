4a4e02841bdf34a8f7d03787c4424d8b5af619de
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2022-12-06 17:24:34 +0000

    passes day 5 part one and two

--- day_5.py
+++ day_5.py
@@ -16,4 +16,40 @@ def load_stacks(filename):
 def load_moves(filename):
     string_moves = [re.findall(r'[0-9]+',move) for move in read_file_of_strings_into_one_string(filename).splitlines()]
     return [[int(move) for move in moves]  for moves in string_moves]
-    
+
+def move_stacks(stacks, move):
+    number_of_crates=move[0]
+    from_stack=move[1] - 1
+    to_stack=move[2] - 1
+    for crate in range(0,number_of_crates):
+        stacks[to_stack] = [stacks[from_stack].pop(0)] + stacks[to_stack]
+    return stacks
+
+def move_stacks_part_two(stacks, move):
+    number_of_crates=move[0]
+    from_stack=move[1] - 1
+    to_stack=move[2] - 1
+    crates_being_moved = []
+    for crate in range(0,number_of_crates):
+        crates_being_moved += [stacks[from_stack].pop(0)]
+    stacks[to_stack] = crates_being_moved + stacks[to_stack]
+    return stacks
+
+def run_moves_and_return_top_crates(moves_filename, stacks_filename):
+    moves = load_moves(moves_filename)
+    stacks = load_stacks(stacks_filename)
+    for move in moves:
+        move_stacks(stacks, move)
+    return [stack[0] for stack in stacks]
+
+def run_moves_and_return_top_crates_part_two(moves_filename, stacks_filename):
+    moves = load_moves(moves_filename)
+    stacks = load_stacks(stacks_filename)
+    for move in moves:
+        move_stacks_part_two(stacks, move)
+    return [stack[0] for stack in stacks]
+
+
+if __name__ == '__main__':
+    print(''.join(run_moves_and_return_top_crates('input/day_5_input_moves.txt','input/day_5_input_stacks.txt')))
+    print(''.join(run_moves_and_return_top_crates_part_two('input/day_5_input_moves.txt','input/day_5_input_stacks.txt')))
--- day_5_test.py
+++ day_5_test.py
@@ -1,9 +1,35 @@
 import pytest
-from day_5 import load_stacks, load_moves
+from day_5 import load_stacks, load_moves, move_stacks, run_moves_and_return_top_crates, move_stacks_part_two
 
 def test_starting_stacks_read_correctly():
     assert load_stacks('input/day_5_test_input_stacks.txt') == [['N', 'Z'], ['D', 'C', 'M'], ['P']]
 
 def test_moves_read_correctly():
     assert load_moves('input/day_5_test_input_moves.txt')[0] == [1,2,1]
+
+def test_first_move_of_single_crate_works():
+    starting_stacks=[['N', 'Z'], ['D', 'C', 'M'], ['P']]
+    first_move = [1,2,1]
+    expected_stacks = [['D','N', 'Z'], ['C', 'M'], ['P']]
+    assert move_stacks(starting_stacks, first_move) == expected_stacks
     
+def test_second_move_of_three_crates_works():
+    starting_stacks=[['D','N', 'Z'], ['C', 'M'], ['P']]
+    first_move = [3,1,3]
+    expected_stacks = [[], ['C', 'M'], ['Z','N', 'D','P']]
+    assert move_stacks(starting_stacks, first_move) == expected_stacks
+
+def test_run_moves_and_return_top_crates_works_for_test_input():
+    assert run_moves_and_return_top_crates('input/day_5_test_input_moves.txt','input/day_5_test_input_stacks.txt') == ['C','M','Z']
+
+def test_first_move_of_single_crate_works_part_2():
+    starting_stacks=[['N', 'Z'], ['D', 'C', 'M'], ['P']]
+    first_move = [1,2,1]
+    expected_stacks = [['D','N', 'Z'], ['C', 'M'], ['P']]
+    assert move_stacks_part_two(starting_stacks, first_move) == expected_stacks
+
+def test_second_move_of_single_crate_works_part_2():
+    starting_stacks=[['D','N', 'Z'], ['C', 'M'], ['P']]
+    first_move = [3,1,3]
+    expected_stacks = [[], ['C', 'M'], ['D','N', 'Z', 'P']]
+    assert move_stacks_part_two(starting_stacks, first_move) == expected_stacks
